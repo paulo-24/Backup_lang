bb3ac1c092a8ea77d2c3079ba3043be8f2cf6432
Abrajano, John Paulo <146899467+paulo-24@users.noreply.github.com>
2024-03-23 15:01:57 +0800

    Update save_employee.php

--- bigbrew/save_employee.php
+++ bigbrew/save_employee.php
@@ -1,19 +1,22 @@
-<?php 
+<?php
 session_start();
 include("php/database.php");
 
-if(isset($_POST['name'], $_POST['employee_id'], $_POST['gender'], $_POST['department'], $_POST['salary'], $_POST['start_date'])) {
+if(isset($_POST['lname'],$_POST['fname'],$_POST['mid'],$_POST['mobile_number'], $_POST['employee_id'], $_POST['gender'], $_POST['department'], $_POST['salary'], $_POST['start_date'])) {
 
-    $name = $_POST['name'];
+    $lname = $_POST['lname'];
+    $fname = $_POST['fname'];
+    $mid = $_POST['mid'];
+    $name = $lname . ', ' . $fname . ' ' . $mid;
+    $mobile_number = $_POST['mobile_number'];
     $employee_id = $_POST['employee_id'];
-     
     $gender = $_POST['gender'];
     $department = implode(',', (array)$_POST['department']); 
     $salary = $_POST['salary'];
     $start_date = $_POST['start_date'];
 
-    $stmt = $connection->prepare("INSERT INTO employees (name, employee_id, gender, department, salary, start_date) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $name, $employee_id, $gender, $department, $salary, $start_date);
+    $stmt = $connection->prepare("INSERT INTO employees (lname, fname, middle_initial, name, mobile_number, employee_id, gender, department, salary, start_date) VALUES (?,?,?,?,?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssss", $lname, $fname, $mid, $name, $mobile_number, $employee_id, $gender, $department, $salary, $start_date);
 
     if ($stmt->execute()) {
         header("Location: positionlist.php");
